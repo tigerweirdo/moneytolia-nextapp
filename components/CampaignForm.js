ce79fb0eff9bb0c6763d83bd121350fb16fbcfca
tigerweirdo <temmuzcetiner@gmail.com>
2024-01-13 23:15:34 +0300

    kampanya yaratma formu oluşturuldu.

---
+++ components/CampaignForm.js
@@ -0,0 +1,125 @@
+import React, { useState , useEffect} from 'react';
+import styles from '../styles/CampaignForm.module.scss';
+
+const CampaignForm = ({ onSubmit }) => {
+    const [campaign, setCampaign] = useState({
+        name: '',
+        description: '',
+        startDate: new Date().toISOString().split('T')[0],
+        targetAudience: '',
+        budget: 0,
+        points: 0,
+    });
+
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+    useEffect(() => {
+        console.log('showSuccessMessage changed:', showSuccessMessage);
+    }, [showSuccessMessage]);
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setCampaign({
+            ...campaign,
+            [name]: name === 'budget' || name === 'points' ? parseInt(value, 10) : value
+        });
+    };
+
+    const handlePointChange = (change) => {
+        setCampaign((prevCampaign) => ({
+            ...prevCampaign,
+            points: Math.max(0, prevCampaign.points + change)
+        }));
+    };
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!campaign.name || !campaign.description) {
+            alert('Name and description are required.');
+            return;
+        }
+
+        onSubmit(campaign);
+        setShowSuccessMessage(true);
+        setTimeout(() => setShowSuccessMessage(false), 2000);
+
+        setCampaign({
+            name: '',
+            description: '',
+            startDate: new Date().toISOString().split('T')[0],
+            endDate: '',
+            targetAudience: '',
+            budget: 0,
+            points: 0,
+        });
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className={styles.campaignForm}>
+            <div className={styles.formHeader}>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Başlık"
+                    value={campaign.name}
+                    onChange={handleChange}
+                    className={styles.formControl}
+                />
+                <input
+                    type="date"
+                    name="startDate"
+                    value={campaign.startDate}
+                    onChange={handleChange}
+                    className={styles.formControl}
+                />
+            </div>
+
+            <div className={styles.formBody}>
+                <textarea
+                    name="description"
+                    placeholder="Açıklama"
+                    value={campaign.description}
+                    onChange={handleChange}
+                    className={styles.formControl}
+                />
+            </div>
+
+            <div className={styles.formFooter}>
+                <input
+                    type="text"
+                    name="targetAudience"
+                    placeholder="Hedef Kitle"
+                    value={campaign.targetAudience}
+                    onChange={handleChange}
+                    className={styles.formControl}
+                />
+                <div className={styles.pointStepper}>
+                    <label htmlFor="points">Puan</label>
+                    <button type="button" onClick={() => handlePointChange(-1)}>-</button>
+                    <input
+                        type="text"
+                        name="points"
+                        readOnly
+                        value={campaign.points}
+                        className={styles.pointDisplay}
+                    />
+                    <button type="button" onClick={() => handlePointChange(1)}>+</button>
+                </div>
+                <input
+  type="text" // Sayısal değerleri işlemek için JavaScript kullanılabilir
+  name="budget"
+  placeholder="Bütçe"
+  value={campaign.budget === 0 ? '' : campaign.budget}
+  onChange={handleChange}
+  className={styles.formControl}
+/>
+            </div>
+
+            <button type="submit" className={styles.submitButton}>Kampanya Yarat</button>
+        </form>
+    );
+};
+
+export default CampaignForm;
